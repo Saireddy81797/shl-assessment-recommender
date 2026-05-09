d3dd5384ad31bfe451eac2af78d1ac999c088e81
Byreddy Sai Reddy <saireddybyreddy72@gmail.com>
2026-05-09 17:15:24 +0530

    Create llm_service.py

---
+++ app/services/llm_service.py
@@ -0,0 +1,14 @@
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+
+def generate_response(prompt):
+    response = model.generate_content(prompt)
+    return response.text
