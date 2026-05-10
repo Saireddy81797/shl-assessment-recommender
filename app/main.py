0ce744e5b230c1985917821311ac581c8325dc49
Byreddy Sai Reddy <saireddybyreddy72@gmail.com>
2026-05-10 11:39:05 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-# from app.routes.chat import router as chat_router
+from app.routes.chat import router as chat_router
 
 app = FastAPI(title="SHL Assessment Recommender")
 
-# app.include_router(chat_router)
+app.include_router(chat_router)
 
 @app.get("/health")
 def health():
